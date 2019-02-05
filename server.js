941e5229c169da33e7eff121abc43ca1a1807257
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-02-05 23:23:42 +0100

    add database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controlers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-horizontal-66964',
         user : 'susan',
         password : 'test',
         database : 'smartbrain'
